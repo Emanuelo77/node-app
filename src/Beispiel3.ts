c4e10824ad63870dcbc7cea14834a607aadd3af7
Emanuel crisan <emanuelcrisan778@gmail.com>
2024-04-16 16:52:41 +0200

    4444

---
+++ src/Beispiel3.ts
@@ -0,0 +1,30 @@
+interface FahrzeugInterface {
+    marke: string;
+    modell: string;
+    baujahr: number;
+    starten(): void;
+    anhalten(): void;
+}
+
+class Auto implements FahrzeugInterface {
+    marke: string;
+    modell: string;
+    baujahr: number;
+
+    constructor(marke: string, modell: string, baujahr: number) {
+        this.marke = marke;
+        this.modell = modell;
+        this.baujahr = baujahr;
+    }
+
+    starten() {
+        console.log("Das Auto startet.");
+    }
+
+    anhalten() {
+        console.log("Das Auto hält an.");
+    }
+}
+
+let meinAuto = new Auto("Ford", "Focus", 2018);
+meinAuto.anhalten();  // Ausgabe: Das Auto hält an.
